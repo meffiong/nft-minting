f24f778b3265464663cd07fc5b0518c631fbe4c9
Mandela Effiong <mandelaeffiong@gmail.com>
2022-12-19 23:38:35 -0600

    First Commit

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 require('babel-register');
 require('babel-polyfill');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
   networks: {
@@ -8,6 +9,14 @@ module.exports = {
       port: 7545,
       network_id: "*" // Match any network id
     },
+    goerli: {
+      host: "localhost",
+      provider: () => new HDWalletProvider("raise where way leaf slot demand pistol common man special mouse lucky", `wss://goerli.infura.io/ws/v3/8bb76c772fc143a9a4f0137d51cf82ee`),
+      port: 8545,
+      network_id: "5",
+      gas: 6700000,
+      gasPrice: 20000000000,
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
